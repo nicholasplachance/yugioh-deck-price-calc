90650add4a7a4b0a05acbc0d7b5871d006da59d3
imberlyarboe <kimberjarboe37@gmail.com>
2020-09-17 00:50:49 -0500

    Add responsive design

--- src/components/Search/Search.component.jsx
+++ src/components/Search/Search.component.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React from "react";
 
-const Search = ({state, onClick, onChange}) => {
-    console.log(onClick)
-    return(
-        <div style={{ display: 'flex', justifyContent: 'center'}}>
-            <form>
-                <input type="text" className="search-box" placeholder={state.header} onChange={onChange}/>
-            </form>
-            <div className="option-menu" style={{ marginTop: "25px"}}>
-            <span>Select a Category</span>
-                <ul className="option-content">
-                {state.options.map( option => (
-            
-                
-                    
-                    <li className="option" key={option.url} onClick={ e => onClick(option)}>{option.header.toUpperCase()}</li>
-                ))}
-                </ul> 
-            </div>
-            </div>
-    )
-}
+const Search = ({ state, onClick, onChange }) => {
+  console.log(onClick);
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <form>
+        <input
+          type="text"
+          className="search-box"
+          placeholder={state.header}
+          onChange={onChange}
+        />
+      </form>
+      <div className="option-menu" style={{ marginTop: "25px" }}>
+        <h6 style={{ margin: "0" }}>Select a Category</h6>
+        <ul className="option-content">
+          {state.options.map((option) => (
+            <li
+              className="option"
+              key={option.url}
+              onClick={(e) => onClick(option)}
+            >
+              {option.header.toUpperCase()}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
